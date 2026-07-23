59715886f3b7d0f3c4bb10f1c2cc554308345e92
kimra151 <147613979+kimra151@users.noreply.github.com>
2026-07-23 15:05:01 +0200

    Delete dresden-titans-hub directory

--- dresden-titans-hub/packages/ui/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  env: { browser: true },
-  ignorePatterns: ['.eslintrc.js', 'dist'],
-};
